f5519a9132ca3cd7cccfedf2675c67cedc982928
chaitanyakumarmelam <melamchaitanyakumar@gmail.com>
2023-07-30 05:11:32 +0530

    Adding workflow changes

--- version.sh
+++ version.sh
@@ -1,65 +1,103 @@
 #!/bin/bash
 
-VERSION=""
+# Define colors for better output visibility
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
 
-# get parameters
+# Function to display a banner with the script name
+function print_banner() {
+    echo -e "${BLUE}============================================${NC}"
+    echo -e "${BLUE}=== Semantic Versioning Tag Incrementer ===${NC}"
+    echo -e "${BLUE}============================================${NC}"
+}
+
+# Function to display usage information
+function print_usage() {
+    echo -e "${GREEN}Usage: $0 -v [Release|hotfix|oschange]${NC}"
+    echo -e "${GREEN}Example: $0 -v oschange${NC}"
+}
+
+# Function to handle incorrect input
+function handle_incorrect_input() {
+    echo -e "${RED}No version type (https://semver.org/) or incorrect type specified, try: -v [Release, hotfix, oschange]${NC}"
+    print_usage
+    exit 1
+}
+
+# Function to handle successful tagging
+function handle_success() {
+    echo -e "${GREEN}Tagged with $NEW_TAG${NC}"
+    git tag $NEW_TAG
+    git push --tags
+    git push
+}
+
+# Function to handle case when a tag already exists on this commit
+function handle_existing_tag() {
+    echo -e "${RED}Already a tag on this commit${NC}"
+}
+
+# Print the banner
+print_banner
+
+# Get parameters
 while getopts v: flag
 do
-  case "${flag}" in
-    v) VERSION=${OPTARG};;
-  esac
+    case "${flag}" in
+        v) VERSION=${OPTARG};;
+    esac
 done
 
-# get highest tag number, and add v0.1.0 if doesn't exist
+# Fetch the highest tag number, and add v0.1.0 if doesn't exist
 git fetch --prune --unshallow 2>/dev/null
 CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-  CURRENT_VERSION='v0.1.0'
+    CURRENT_VERSION='v0.1.0'
 fi
-echo "Current Version: $CURRENT_VERSION"
+echo -e "${GREEN}Current Version: $CURRENT_VERSION${NC}"
 
-# replace . with space so can split into an array
+# Replace . with space so can split into an array
 CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
 
-# get number parts
+# Get number parts
 VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
+# Increment version numbers based on input
 if [[ $VERSION == 'Release' ]]
 then
-  VNUM1=v$((VNUM1+1))
-elif [[ $VERSION == 'Hotfix' ]]
+    VNUM1=v$((VNUM1+1))
+elif [[ $VERSION == 'hotfix' ]]
 then
-  VNUM2=$((VNUM2+1))
-elif [[ $VERSION == 'patch' ]]
+    VNUM2=$((VNUM2+1))
+elif [[ $VERSION == 'oschange' ]]
 then
-  VNUM3=$((VNUM3+1))
+    VNUM3=$((VNUM3+1))
 else
-  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [Release, Hotfix, patch]"
-  exit 1
+    handle_incorrect_input
 fi
 
-# create new tag
+# Create new tag
 NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
-echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
+echo -e "${GREEN}($VERSION) updating $CURRENT_VERSION to $NEW_TAG${NC}"
 
-# get current hash and see if it already has a tag
+# Get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
 NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
-# only tag if no tag already
+# Only tag if no tag already (also check NEEDS_TAG here)
 if [ -z "$NEEDS_TAG" ]; then
-  echo "Tagged with $NEW_TAG"
-  git tag $NEW_TAG
-  git push --tags
-  git push
+    handle_success
 else
-  echo "Already a tag on this commit"
+    handle_existing_tag
 fi
 
 echo ::set-output name=git-tag::$NEW_TAG
 
 exit 0
+
